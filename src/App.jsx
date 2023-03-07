eb4a86362962abb2d25fe689cfc609508aec9042
kiranFullStack <kiran.fullstack@outlook.com>
2023-03-07 17:03:24 +0530

    starter project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Redux todo build upon the todo app</h1>
+    </div>
+  )
+}
+
+export default App
