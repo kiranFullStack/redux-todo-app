88844216f273a2e986c5a0324800e20f9cacb01f
kiranFullStack <kiran.fullstack@outlook.com>
2023-03-07 17:11:27 +0530

    Implemented Redux as Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import { Counter } from './Counter'
 
 function App() {
   return (
     <div className='App'>
       <h1>Redux todo build upon the todo app</h1>
+      <Counter />
     </div>
   )
 }
